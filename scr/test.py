03155f4cf2a2b0418ffbd109a841ce16dbd7385c
Håkon Muggerud <haakontm@stud.ntnu.no>
2021-11-09 16:01:02 +0100

    Finishing ex1. Still eta_scaling remaining

--- scr/test.py
+++ scr/test.py
@@ -1,17 +1,23 @@
 import numpy as np
 from sklearn.linear_model import SGDRegressor
 
-def SGD_linreg(X, y, n_epochs, batch_size, learning_rate):
-    beta = np.random.randn(X.shape[1])
-    n_batches = len(y) // batch_size
+def SGD_linreg(X, y, n_epochs, batch_size, eta, eta_scaling='constant'):
+    datapoints = X.shape[0]
+    beta = np.random.randn(X.shape[1], 1)
+    eta0 = eta
     for epoch in range(n_epochs):
-        for i in range(n_batches):
-            batch_idx = np.random.randint(n_batches * batch_size - batch_size)
-            x_i = X[batch_idx:batch_idx + batch_size]
-            y_i = y[batch_idx:batch_idx + batch_size]
-            grad = 2 * x_i.T @ ((x_i @ beta) - y_i)
-            step_length = 1 / (epoch * n_batches + i + 1 / learning_rate)
-            beta = beta - step_length * grad
+        perm = np.random.permutation(datapoints)
+        X_shuffled = X[perm, :]
+        y_shuffled = y[perm, :]
+        for i in range(0, datapoints, batch_size):
+            x_i = X_shuffled[i:i+batch_size, :]
+            y_i = y_shuffled[i:i+batch_size, :]
+            if eta_scaling == 'optimal':
+                t = epoch * (datapoints // batch_size) + i // batch_size
+                eta = 1 / (t + (1 / eta0))
+            # print("Current learning rate: ", eta)
+            gradient = (2 / batch_size) * x_i.T @ ((x_i @ beta) - y_i)
+            beta = beta - gradient * eta
     return beta
 
 m = 100
@@ -22,10 +28,10 @@ X = np.c_[np.ones((m,1)), x]
 theta_linreg = np.linalg.inv(X.T @ X) @ (X.T @ y)
 print("Own inversion")
 print(theta_linreg)
-sgdreg = SGDRegressor(max_iter = 100, eta0=0.1)
+sgdreg = SGDRegressor()
 sgdreg.fit(x,y.ravel())
 print("sgdreg from scikit")
 print(sgdreg.intercept_, sgdreg.coef_)
 
-betas = SGD_linreg(X, y.ravel(), 100, 3, 0.1)
+betas = SGD_linreg(X, y.reshape(-1, 1), 100, 10, 0.5, eta_scaling='optimal')
 print(betas)
