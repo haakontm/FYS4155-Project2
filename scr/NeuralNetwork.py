af1405c934af0ee10f061f9a392c81b3e27206cb
hakontm <hakontm@gmail.com>
2021-11-13 11:12:56 +0100

    More progress kind of

--- scr/NeuralNetwork.py
+++ scr/NeuralNetwork.py
@@ -2,23 +2,86 @@ import numpy as np
 
 class NeuralNetwork:
 
-    def __init__(self, layers, activation_func='sigmoid'):
+    def __init__(self, 
+                 layers, 
+                 activation_func='sigmoid', 
+                 epochs=100, 
+                 mini_batch_size=1,
+                 eta=0.01,
+                 eta_scaling='constant'):
         self.layers = layers
         self.n_layers = len(layers)
         self.weights = [np.random.randn(y, x) for x, y in zip(layers[:-1], layers[1:])]
         self.biases = [np.random.randn(x, 1) for x in layers[1:]]
+        self.epochs = epochs
+        self.batch_size = mini_batch_size
+        self.eta = eta
+        self.eta_scaling = eta_scaling
 
         if activation_func == 'sigmoid':
             self.activation_func = lambda z : 1.0/(1.0+np.exp(-z))
+            self.d_activation_func = lambda z : self.activation_func(z) * (1 - self.activation_func(z))
 
     
-    def predict(self, X):    
+    def predict(self, X):
+        "This function assumes that each column is an entry in the dataset(rows are features)"
         for b, w in zip(self.biases, self.weights):
             X = self.activation_func(w @ X - b)
-            #print(X)
         return X
 
 
+    def fit(self, X, y):
+        datapoints = X.shape[0]
+        for epoch in range(self.epochs):
+            perm = np.random.permutation(datapoints)
+            X_shuffled = X[perm, :]
+            y_shuffled = y[perm, :]
+            for i in range(0, datapoints, self.batch_size):
+                x_i = X_shuffled[i:i+self.batch_size, :]
+                y_i = y_shuffled[i:i+self.batch_size, :]
+                if self.eta_scaling == 'optimal':
+                    t = epoch * (datapoints // self.batch_size) + i // self.batch_size
+                    self.eta = 1 / (t + 10)
+                self.step(x_i, y_i)
+
+
+    def step(self, X, y):
+        "Update the weights and biases in the network with computed gradients from a minibatch"
+        nab_w = [np.zeros(w.shape) for w in self.weights]
+        nab_b = [np.zeros(b.shape) for b in self.biases]
+        for i in range(self.batch_size):
+            delta_nab_b, delta_nab_w = self.backpropagation(X[i, :], y[i, :])
+            nab_w = [nw + dnw for nw, dnw in zip(nab_w, delta_nab_w)]
+            nab_b = [nb + dnb for nb, dnb in zip(nab_b, delta_nab_b)]
+        self.weights = [w - (self.eta / self.batch_size) * nw for w, nw in zip(self.weights, nab_w)]
+        self.biases = [b - (self.eta / self.batch_size) * nb for b, nb in zip(self.biases, nab_b)]
+
+    def backpropagation(self, X, y):
+        nab_w = [np.zeros(w.shape) for w in self.weights]
+        nab_b = [np.zeros(b.shape) for b in self.biases]
+        a = X
+        a_values = [X]
+        z_values = []
+
+        for b, w, in zip(self.biases, self.weights):
+            z = w @ a + b
+            z_values.append(z)
+            a = self.activation_func(z)
+            a_values.append(a)
+        
+        delta = (a_values[-1] - y) * self.d_activation_func(z_values[-1])
+        nab_w[-1] = delta @ a_values[-2].T
+        nab_b[-1] = delta
+
+        for l in range(2, self.n_layers):
+            print(z_values[-l + 2].shape)
+            z = z_values[-l]
+            d_a = self.d_activation_func(z)
+            delta = (self.weights[-l + 1].T @ delta) * d_a
+            print(delta.shape, a_values[-l - 1].shape)
+            nab_w[-l] = delta @ a_values[-l - 1].T
+            nab_b[-l] = delta
+        return (nab_b, nab_w)
 
 
 
@@ -64,7 +127,8 @@ X = create_X(x, y, polygrad)
 
 X_train, X_test, z_train, z_test = train_test_split(X, z.reshape(-1, 1), test_size=0.3)
 
-network = NeuralNetwork([X_train.shape[1], 5, 10, 1])
+network = NeuralNetwork([X_train.shape[1], 3, 1], mini_batch_size=15, eta=0.0001)
+network.fit(X_train, z_train)
 pred = network.predict(X_test.T)
 
 #print(network.weights, network.biases)
--- scr/test.py
+++ scr/test.py
@@ -1,37 +1,166 @@
 import numpy as np
-from sklearn.linear_model import SGDRegressor
+from sklearn.neural_network import MLPRegressor
 
-def SGD_linreg(X, y, n_epochs, batch_size, eta, eta_scaling='constant'):
-    datapoints = X.shape[0]
-    beta = np.random.randn(X.shape[1], 1)
-    eta0 = eta
-    for epoch in range(n_epochs):
-        perm = np.random.permutation(datapoints)
-        X_shuffled = X[perm, :]
-        y_shuffled = y[perm, :]
-        for i in range(0, datapoints, batch_size):
-            x_i = X_shuffled[i:i+batch_size, :]
-            y_i = y_shuffled[i:i+batch_size, :]
-            if eta_scaling == 'optimal':
-                t = epoch * (datapoints // batch_size) + i // batch_size
-                eta = 1 / (t + (1 / eta0))
-            # print("Current learning rate: ", eta)
-            gradient = (2 / batch_size) * x_i.T @ ((x_i @ beta) - y_i)
-            beta = beta - gradient * eta
-    return beta
+class NeuralNetwork:
+    def __init__(
+            self,
+            X_data,
+            Y_data,
+            n_hidden_neurons=50,
+            n_categories=10,
+            epochs=10,
+            batch_size=100,
+            eta=0.1,
+            lmbd=0.0):
 
-m = 100
-x = 2*np.random.rand(m,1)
-y = 4+3*x+np.random.randn(m,1)
+        self.X_data_full = X_data
+        self.Y_data_full = Y_data
 
-X = np.c_[np.ones((m,1)), x]
-theta_linreg = np.linalg.inv(X.T @ X) @ (X.T @ y)
-print("Own inversion")
-print(theta_linreg)
-sgdreg = SGDRegressor()
-sgdreg.fit(x,y.ravel())
-print("sgdreg from scikit")
-print(sgdreg.intercept_, sgdreg.coef_)
+        self.n_inputs = X_data.shape[0]
+        self.n_features = X_data.shape[1]
+        self.n_hidden_neurons = n_hidden_neurons
+        self.n_categories = n_categories
 
-betas = SGD_linreg(X, y.reshape(-1, 1), 100, 10, 0.5, eta_scaling='optimal')
-print(betas)
+        self.epochs = epochs
+        self.batch_size = batch_size
+        self.iterations = self.n_inputs // self.batch_size
+        self.eta = eta
+        self.lmbd = lmbd
+
+        self.create_biases_and_weights()
+
+    def create_biases_and_weights(self):
+        self.hidden_weights = np.random.randn(self.n_features, self.n_hidden_neurons)
+        self.hidden_bias = np.zeros(self.n_hidden_neurons) + 0.01
+
+        self.output_weights = np.random.randn(self.n_hidden_neurons, self.n_categories)
+        self.output_bias = np.zeros(self.n_categories) + 0.01
+
+    def feed_forward(self):
+        # feed-forward for training
+        self.z_h = np.matmul(self.X_data, self.hidden_weights) + self.hidden_bias
+        self.a_h = sigmoid(self.z_h)
+
+        print(self.a_h)
+
+        self.z_o = np.matmul(self.a_h, self.output_weights) + self.output_bias
+
+        exp_term = np.exp(self.z_o)
+        self.probabilities = exp_term / np.sum(exp_term, axis=1, keepdims=True)
+
+    def feed_forward_out(self, X):
+        # feed-forward for output
+        z_h = np.matmul(X, self.hidden_weights) + self.hidden_bias
+        a_h = sigmoid(z_h)
+
+        z_o = np.matmul(a_h, self.output_weights) + self.output_bias
+        
+        exp_term = np.exp(z_o)
+        print(exp_term)
+        probabilities = exp_term / np.sum(exp_term, axis=1, keepdims=True)
+        return probabilities
+
+    def backpropagation(self):
+        error_output = self.probabilities - self.Y_data
+        error_hidden = np.matmul(error_output, self.output_weights.T) * self.a_h * (1 - self.a_h)
+
+        self.output_weights_gradient = np.matmul(self.a_h.T, error_output)
+        self.output_bias_gradient = np.sum(error_output, axis=0)
+
+        self.hidden_weights_gradient = np.matmul(self.X_data.T, error_hidden)
+        self.hidden_bias_gradient = np.sum(error_hidden, axis=0)
+
+        if self.lmbd > 0.0:
+            self.output_weights_gradient += self.lmbd * self.output_weights
+            self.hidden_weights_gradient += self.lmbd * self.hidden_weights
+
+        self.output_weights -= self.eta * self.output_weights_gradient
+        self.output_bias -= self.eta * self.output_bias_gradient
+        self.hidden_weights -= self.eta * self.hidden_weights_gradient
+        self.hidden_bias -= self.eta * self.hidden_bias_gradient
+
+    def predict(self, X):
+        probabilities = self.feed_forward_out(X)
+        return np.argmax(probabilities, axis=1)
+
+    def predict_probabilities(self, X):
+        return self.feed_forward_out(X)
+
+    def train(self):
+        data_indices = np.arange(self.n_inputs)
+
+        for i in range(self.epochs):
+            for j in range(self.iterations):
+                # pick datapoints with replacement
+                chosen_datapoints = np.random.choice(
+                    data_indices, size=self.batch_size, replace=False
+                )
+
+                # minibatch training data
+                self.X_data = self.X_data_full[chosen_datapoints]
+                self.Y_data = self.Y_data_full[chosen_datapoints]
+
+                self.feed_forward()
+                self.backpropagation()
+    
+
+def sigmoid(x):
+    return 1/(1 + np.exp(-x))
+
+
+def FrankeFunction(x,y):
+    term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
+    term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
+    term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2)) 
+    term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
+    return term1 + term2 + term3 + term4
+
+def create_X(x, y, n):
+	if len(x.shape) > 1:
+		x = np.ravel(x)
+		y = np.ravel(y)
+
+	N = len(x)
+	l = int((n+1)*(n+2)/2)		# Number of elements in beta
+	X = np.ones((N,l))
+
+	for i in range(1,n+1):
+		q = int((i)*(i+1)/2)
+		for k in range(i+1):
+			X[:,q+k] = (x**(i-k))*(y**k)
+
+	return X
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_squared_error
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+polygrad = 10
+N = 20  # Number of points
+noise = 0.05
+
+x = np.linspace(0, 1, N)
+y = np.linspace(0, 1, N)
+x, y = np.meshgrid(x, y)
+
+z = FrankeFunction(x, y)
+z += noise * np.random.randn(z.shape[0], z.shape[1])
+
+X = create_X(x, y, polygrad)
+
+X_train, X_test, z_train, z_test = train_test_split(X, z.reshape(-1, 1), test_size=0.3)
+
+#net = MLPRegressor(activation='logistic', alpha=0, learning_rate_init=0.1, batch_size=20, hidden_layer_sizes=(30,20), max_iter=1000)
+#net.fit(X_train, z_train)
+#pred = net.predict(X_test)
+#surface = net.predict(X).reshape(z.shape)
+
+network = NeuralNetwork(X_train, z_train, 20, 1, 2, 15, 0.0001)
+network.train()
+pred = network.predict_probabilities(X_test)
+print(pred)
+#surface = network.predict(X).reshape(z)
+
+#print(network.weights, network.biases)
+print(r2_score(z_test, pred.ravel()))
